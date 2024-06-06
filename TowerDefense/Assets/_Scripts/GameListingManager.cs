89e3c2742a592cec4832fc43e71166e872336db4
Lucas Juel <lucas.juel.sjostrom@gmail.com>
2024-06-06 15:58:25 +0200

    fix refresh, and list update

--- TowerDefense/Assets/_Scripts/GameListingManager.cs
+++ TowerDefense/Assets/_Scripts/GameListingManager.cs
@@ -13,23 +13,28 @@ public class GameListingManager : MonoBehaviourPunCallbacks
     void Start(){
         Debug.Log("GameListingManager started");
     }
+
+    private int DoesRoomExist(RoomInfo room){
+        int index = _listings.FindIndex(x => x._roomInfo.Name == room.Name);
+        return index;
+    }
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
         Debug.Log("Room list updated");
         foreach (RoomInfo room in roomList)
         { 
+            int index = DoesRoomExist(room);
+            Debug.Log("Room: " + room.Name + " indeX: " + index);
             if(room.RemovedFromList){
-                int index = _listings.FindIndex(x => x._roomInfo.Name == room.Name);
                 if (index != -1){
                     Destroy(_listings[index].gameObject);
                     _listings.RemoveAt(index);
                 }
                 continue;
-            } else if(room.IsVisible){
+            } else if(room.IsVisible && index == -1){
                 GameListing listing = Instantiate(_gameListing, _content);
                 listing.SetGameInfo(room, room.Name, room.MaxPlayers, room.PlayerCount);
-            } else if(!room.IsVisible){
-                Debug.Log("Found non visible room");
+                _listings.Add(listing);
             }
         }
     }
--- TowerDefense/Assets/_Scripts/Multiplayer/CreateOrJoinRoom.cs
+++ TowerDefense/Assets/_Scripts/Multiplayer/CreateOrJoinRoom.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 using Photon.Pun;
 using TMPro;
 using UnityStandardAssets.Characters.ThirdPerson.PunDemos;
+using Photon.Realtime;
 
 public class CreateOrJoinLobby : MonoBehaviourPunCallbacks
 {
-
+    [SerializeField] private float refreshInterval = 5f; // Opdateringsinterval i sekunder
     public TMP_InputField createRoomInputField;
     private GameObject _content;
     // Remember that when creating a room, the player automatically and immediately joins that room
@@ -16,6 +17,16 @@ public class CreateOrJoinLobby : MonoBehaviourPunCallbacks
     {
         Debug.Log("Creating room");
         PhotonNetwork.CreateRoom(createRoomInputField.text, new Photon.Realtime.RoomOptions {PublishUserId = true});
+    }
+
+    void Start(){
+        Debug.Log("Start");
+        RefreshRoomList();
+    }
+    public void RefreshRoomList(){
+        PhotonNetwork.LeaveLobby();
+        PhotonNetwork.JoinLobby();
+        Debug.Log("Room list refreshed");
     }
 
     public override void OnJoinedRoom()
